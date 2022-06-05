6e6f57c17345c5499ab2e30f5c44351c4a57b18b
evekites <erik@verberne.net>
2022-06-05 22:48:00 +0200

    Add files via upload

---
+++ 013 range finder/main.py
@@ -0,0 +1,11 @@
+from microbit import *
+from ultrasonic import *
+
+rf = Rangefinder(pin0)
+
+display.show(Image.YES)
+
+while True:
+    dist = rf.distance_cm()
+    print(int(dist))
+    sleep(10)
